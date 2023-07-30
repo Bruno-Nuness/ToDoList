b71b16dbc185dbb755ed6be2591d105905fe0700
Brun0Nun3s <bruunofernandonunes@gmail.com>
2023-07-29 21:05:26 -0300

    feat: update and get

--- back/src/app.ts
+++ back/src/app.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import "express-async-error";
 import express from "express";
 import userRoutes from "./routes/users.routes";
-
+import cors from "cors";
 import { handleErrors } from "./errors/AppError";
 import { loginRoutes } from "./routes/login.routes";
 import { taskRoutes } from "./routes/task.routes";
@@ -10,6 +10,7 @@ import { taskRoutes } from "./routes/task.routes";
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use("/login", loginRoutes);
 app.use("/users", userRoutes);
 app.use("/tasks", taskRoutes);
